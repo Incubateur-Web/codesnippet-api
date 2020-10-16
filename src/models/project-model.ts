de526c8ebfe9f7df2f0f6a61a985a4dd9a583e31
Miki Namsrai <miki.namsrai@gmail.com>
2020-10-16 10:56:30 +0200

    add project schema and attributes

---
+++ src/models/project-model.ts
@@ -0,0 +1,101 @@
+import { Document, Model, Mongoose, Schema } from 'mongoose';
+import ServiceContainer from '../services/service-container';
+import Attributes from './model';
+
+/**
+ * Project attributes interface.
+ */
+export interface ProjectAttributes extends Attributes {
+    title: string;
+    description: string;
+    avatar: string;
+    idAWS: number;
+    idOwner: number;
+    adminsList: string[];
+    editorsList: string[];
+    readersList: string[];
+    filesList: string[];
+    foldersList: string[];
+}
+
+/**
+ * Project instance interface.
+ */
+export interface ProjectInstance extends ProjectAttributes, Document {}
+
+/**
+ * Creates the project model.
+ * 
+ * @param container Services container
+ * @param mongoose Mongoose instance
+ */
+export default function createModel(container: ServiceContainer, mongoose: Mongoose): Model<ProjectInstance> {
+    return mongoose.model('Project', createProjectSchema(container), 'projects');
+}
+
+/**
+ * Creates the project schema.
+ * 
+ * @param container Services container
+ * @returns Project schema
+ */
+function createProjectSchema(container: ServiceContainer) {
+    const schema = new Schema({
+        title: {
+            type: Schema.Types.String,
+            required: [true, 'A title is required'],
+        },
+        description: {
+            type: Schema.Types.String,
+            required: [true, 'A description is required'],
+        },
+        avatar: {
+            type: Schema.Types.String,
+            required: [false, ''],
+        },
+        idAWS: {
+            type: Schema.Types.Number,
+            required: [false, ''],
+        },
+        idOwner: {
+            type: Schema.Types.Number,
+            required: [false, ''],
+        },
+        adminsList: {
+          type: [{
+            type: Schema.Types.String
+          }],
+            required: [false, ''],
+        },
+        editorsList: {
+          type: [{
+            type: Schema.Types.String
+          }],
+            required: [false, ''],
+        },
+        readersList: {
+          type: [{
+            type: Schema.Types.String
+          }],
+            required: [false, ''],
+        },
+        filesList: {
+          type: [{
+            type: Schema.Types.String
+          }],
+            required: [false, ''],
+        },
+        foldersList: {
+          type: [{
+            type: Schema.Types.String
+          }],
+            required: [false, ''],
+        },
+    }, {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    });
+
+    return schema;
+}
